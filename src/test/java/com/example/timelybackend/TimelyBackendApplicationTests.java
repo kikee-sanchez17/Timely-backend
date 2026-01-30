5010f622fcc6dd6508cff17aab7d671a10f46e02
kikee-sanchez17 <enricmontoya@gmail.com>
2026-01-30 11:26:39 +0100

    first commit

---
+++ src/test/java/com/example/timelybackend/TimelyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.timelybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimelyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
