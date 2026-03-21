de892089297db5046f8f4922a729e2a59be82089
kikee-sanchez17 <enricmontoya@gmail.com>
2026-03-21 20:50:31 +0100

    chore: backend base structure and package organization

--- src/main/java/dev/esanchez/timely/backend/TimelyBackendApplication.java
+++ src/main/java/dev/esanchez/timely/backend/TimelyBackendApplication.java
@@ -1,4 +1,4 @@
-package com.example.timelybackend;
+package dev.esanchez.timely.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/dev/esanchez/timely/backend/TimelyBackendApplicationTests.java
+++ src/test/java/dev/esanchez/timely/backend/TimelyBackendApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.timelybackend;
+package dev.esanchez.timely.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
